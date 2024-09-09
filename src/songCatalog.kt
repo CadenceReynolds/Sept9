b3ca20083ae7ff1845db15bc88fcb3a7166d745e
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-09-09 12:17:23 -0400

    September 9th first two Assignments in class work.

--- src/songCatalog.kt
+++ src/songCatalog.kt
@@ -1,2 +1,19 @@
-class songCatalog {
+fun main() {
+    val song = songCatalog("War Pigs", "Jhone Benever", 1968, 1100)
+    song.printDescription()
+}
+
+class songCatalog(
+    val title: String,
+    val artist: String,
+    val yearPublished: Int,
+    val playCount: Int
+) {
+    val isPopular: Boolean
+        get() = playCount >= 1000
+
+
+    fun printDescription() {
+        println("$title, preformed by $artist, was released in $yearPublished years published")
+    }
 }
